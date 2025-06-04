5479ef983a50678b299f2c86102bebc39855135f
kritgarg <kritg0160@gmail.com>
2025-06-04 20:40:16 +0530

    editedbackend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:8080', 'http://127.0.0.1:8080'], // Vite's default ports and custom port
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'https://cs-order-management.netlify.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
